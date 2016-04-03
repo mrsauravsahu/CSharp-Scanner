4dfdbea000d4a226db7df20c3d078c50b2badce7
sauravMSFT <sauravsahu.12@live.com>
2016-04-03 23:34:08 +0530

    Added the Scanner class. This the first Build, so it has some issues. 1. Thoughts like throwing custom exceptions or predefined exceptions by .NET are yet to be discussed. 2. Optimization can be made regarding reading from source and removing empty entries into the values List.

---
+++ Scanner.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace System
+{
+    class Scanner
+    {
+        private List<object> values = new List<object>();
+        private string delimiter;
+        private TextReader inputStream;
+
+        public Scanner(string delimiter = " ")
+        {
+            this.delimiter = delimiter;
+            this.inputStream = Console.In;
+        }
+        public Scanner(TextReader inputStream, string delimiter = " ")
+        {
+            this.delimiter = delimiter;
+            this.inputStream = inputStream;
+        }
+
+        private void readIfNecessary()
+        {
+            if (values.Count == 0)
+            {
+                values.AddRange((Regex.Split(inputStream.ReadLine(), delimiter)));
+                values.RemoveAll(p => string.Compare(p.ToString(), "") == 0);
+            }
+        }
+        public bool HasNext()
+        {
+            return (values.Count > 0);
+        }
+        public bool HasNext<T>() where T : IConvertible
+        {
+            T value;
+            if (values.Count == 0) throw new EndOfStreamException("Input Buffer was empty. No input was provided");
+            value = (T)Convert.ChangeType(values[0], typeof(T));
+            return true;
+        }
+        public T ReadNext<T>() where T : IConvertible
+        {
+            T value;
+            ReadNext<T>(out value);
+            return value;
+        }
+        public void ReadNext<T>(out T value) where T : IConvertible
+        {
+            readIfNecessary();
+            if (values.Count == 0) throw new EndOfStreamException("Input Buffer was empty. No input was provided");
+            value = (T)Convert.ChangeType(values[0], typeof(T));
+            values.RemoveAt(0);
+        }
+        public List<T> ReadAllNext<T>() where T : IConvertible
+        {
+            List<T> all;
+            ReadAllNext<T>(out all);
+            return all;
+        }
+        public void ReadAllNext<T>(out List<T> values) where T : IConvertible
+        {
+            readIfNecessary();
+            values = new List<T>();
+            while (HasNext<T>())
+            {
+                if (this.values.Count == 0) throw new EndOfStreamException("Input Buffer was empty. No input was provided");
+                values.Add((T)Convert.ChangeType(this.values[0], typeof(T)));
+                this.values.RemoveAt(0);
+            }
+        }
+    }
+}
