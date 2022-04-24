ef5154a089a07c2fe1471df505b0209ac9955935
Saurav Sahu <mrsauravsahu@outlook.com>
2022-04-24 20:42:24 +0530

    add an example console app - playground

---
+++ examples/playground/Program.cs
@@ -0,0 +1,10 @@
+﻿using mrsauravsahu.scanner;
+
+var scanner = new Scanner();
+
+Console.WriteLine($"Enter some numbers. (Example: \"1 2 3\")");
+
+var value = scanner.ReadNext<int>();
+
+Console.WriteLine($"Next oncoming int is - {value}");
+Console.WriteLine($"Type - {value.GetType()}");
